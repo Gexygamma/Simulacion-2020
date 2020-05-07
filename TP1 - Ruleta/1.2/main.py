defa7b66a3a6ecaa5a63888a541e2df26c8863df
Francisco <49844500+Gexygamma@users.noreply.github.com>
2020-05-07 19:45:38 -0300

    Descartado metodo de puntaje

--- TP1 - Ruleta/1.2/main.py
+++ TP1 - Ruleta/1.2/main.py
@@ -19,47 +19,13 @@ def sim_estrategia():
 
     sim = Simulacion(cantidad_tiradas, tirada_favorable)
     sim.agregar_estrategia(
-        LabouchereInvertida([1,2,3])
+        Martingala(1),
+        MartingalaGrande(1),
+        Labouchere([1,2,3,4]),
+        Dalembert(4)
     )
     sim.ejecutar()
     graficar_simulacion(sim)
-
-def calc_puntaje():
-    """ Simula varias estrategias en paralelo y mide su desempeño. """
-    cantidad_tiradas = 2000
-    iteraciones = 5000
-    tirada_favorable = lambda tirada : tirada.color == Color.ROJO
-
-    dict_puntaje = {}
-    for i in range(iteraciones):
-        sim = Simulacion(cantidad_tiradas, tirada_favorable)
-        sim.agregar_estrategia(
-            Martingala(1),
-            MartingalaGrande(1),
-            MartingalaInvertida(1),
-            MartingalaInvertida(1, 30),
-            Labouchere([1,2,3]),
-            LabouchereInvertida([1,2,3]),
-            Dalembert(4),
-            DalembertInvertida(4),
-            DalembertInvertida(4, 4)
-        )
-        sim.ejecutar()
-        for estrategia in sim.estrategias:
-            if i == 0:
-                dict_puntaje[str(estrategia)] = []
-            barrera_absorcion = estrategia.barrera_absorcion[-1]
-            rendimiento = estrategia.rendimiento[-1]
-            dict_puntaje[str(estrategia)].append(rendimiento / barrera_absorcion)
-        if i % (iteraciones / 100) == 0:
-            percentage = i/(iteraciones/100)
-            print("Cargando... ", percentage, "%")
-            if percentage % 10 == 0 and percentage != 0:
-                print('...')
-                sleep(15) # Esto esta xq sino el ventilador de mi cpu explota. :(
-    print("Fin de la simulación...")
-    for str_estrategia, lista_puntaje in dict_puntaje.items():
-        print("{:>28}: {:9.6f}".format(str_estrategia, sum(lista_puntaje)/len(lista_puntaje)))
 
 if __name__ == '__main__':
     sim_estrategia()
