77e9332b42f36b2ff3fca5f88c83f270afbf951b
Francisco <49844500+Gexygamma@users.noreply.github.com>
2020-05-17 05:51:38 -0300

    Implementado test chi-cuadrado

---
+++ TP2 - PRNG/2.1/archivos/test/chi_cuadrado.py
@@ -0,0 +1,38 @@
+from scipy.stats import chi2
+from math import floor
+
+def chi_cuadrado(generador, n_interv, a=0.05):
+    """
+    Implementación de prueba de chi cuadrado (frecuencia). Verifica si el generador
+    no es rechazado por la hipotesis nula.
+
+    Args:
+        generador: Generador de nros aleatorios a probar.
+        n_interv: Cantidad de intervalos.
+        a: Nivel de significancia.
+    """
+
+    # Obtener muestra e inicializar variables
+    muestra = list(generador)
+    n_obsv = len(muestra)
+    gr_lib = n_interv - 1
+    frec_esperada = n_obsv / n_interv
+
+    # Excepción para asegurar que todos los intervalos tengan la misma frec esperada.
+    if n_obsv % n_interv != 0:
+        raise ValueError("Observaciones debe ser múltiplo de intervalos.")
+    
+    # Poblar intervalos.
+    intervalos = [0] * n_interv
+    for elemento in muestra:
+        index = floor(elemento * n_interv)
+        intervalos[index] += 1
+
+    # Calcular valor chi según fórmula.
+    chi = sum(((frec_observada - frec_esperada)**2 / frec_esperada) \
+        for frec_observada in intervalos)
+
+    # Obtener valor crítico desde librería ("tabla").
+    valor_critico = chi2.isf(q=a, df=gr_lib)
+
+    return chi < valor_critico
--- TP2 - PRNG/2.1/main.py
+++ TP2 - PRNG/2.1/main.py
@@ -1,9 +1,16 @@
 from archivos.generadores.lcg import lcg
 from archivos.generadores.middle_square import middle_square
+from archivos.test.chi_cuadrado import chi_cuadrado
 
 if __name__ == "__main__":
-    for i in lcg(5):
-        print(i)
-    
-    for i in middle_square(5):
-        print(i)
+    print("Prueba Chi-Cuadrado con LCG.")
+    for _i in range(4):
+        result = chi_cuadrado(lcg(100), 10)
+        msg = "Pasó" if result else "Falló"
+        print(msg)
+
+    print("Prueba Chi-Cuadrado con cuadrado medio.")
+    for _i in range(4):
+        result = chi_cuadrado(middle_square(100), 10)
+        msg = "Pasó" if result else "Falló"
+        print(msg)
