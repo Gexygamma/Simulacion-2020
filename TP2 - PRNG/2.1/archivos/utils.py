affd568600629ac9f51526d2b8d21b7c05ace3e7
Francisco <49844500+Gexygamma@users.noreply.github.com>
2020-05-23 02:22:48 -0300

    Agregado pairwise del módulo "more-itertools"

---
+++ TP2 - PRNG/2.1/archivos/utils.py
@@ -0,0 +1,9 @@
+from itertools import tee
+
+# Función obtenida de la sección de recetas de itertools.
+# https://docs.python.org/3/library/itertools.html
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
